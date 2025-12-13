39e9c1941338db234aa82830e65ea885af60b0b9
Felipe Bressianini <felipebressianini@gmail.com>
2025-12-13 15:07:46 -0300

    First migration using Alembic

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,85 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fastapi_zero.app import app
-
-
-def test_root_should_return_hello_world():
-    client = TestClient(app)
-
+def test_root_should_return_hello_world(client):
     response = client.get('/')
 
     assert response.json() == {'message': 'Hello, World!'}
     assert response.status_code == HTTPStatus.OK
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'Alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'email': 'alice@example.com',
+        'username': 'Alice',
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [{'id': 1, 'email': 'alice@example.com', 'username': 'Alice'}]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        'users/1',
+        json={
+            'username': 'Bob',
+            'email': 'bob@example.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'Bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
+
+
+def test_update_user_not_found(client):
+    response = client.put(
+        'users/2',
+        json={
+            'username': 'Bob',
+            'email': 'bob@example.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json()['detail'] == 'User not found!'
+
+
+def test_delete_user(client):
+    response = client.delete('users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'Bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
+
+
+def test_delete_user_not_found(client):
+    response = client.delete('users/2')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json()['detail'] == 'User not found!'
